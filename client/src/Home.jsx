65d05fb03c05fcb875612076c1c7428e0f180346
Debajit Giri <152266134+Debjit69@users.noreply.github.com>
2025-07-18 11:46:00 +0530

    Update Home.jsx

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,18 +1,19 @@
 import React, {useEffect,useState} from 'react'
 import Create from './Create'
 import axios from 'axios'
+import {api} from "config.js"
 import { BsCircleFill, BsFillCheckCircleFill, BsFillTrashFill } from 'react-icons/bs';
 
 function Home ()  {
     const [todos,setTodos]=useState([])
     useEffect(() => {
-      axios.get('http://localhost:3001/get')
+      axios.get(api('get'))
       .then(result => setTodos(result.data))
       .catch(err => console.log(err))
     },[])
 
     const handleEdit = (id) => {
-      axios.put('http://localhost:3001/update/' + id)
+      axios.put(api('/update') + id)
       .then(result => {
         location.reload()
       })
@@ -21,7 +22,7 @@ function Home ()  {
     }
 
     const handleDelete = (id) => {
-      axios.delete('http://localhost:3001/delete/' + id)
+      axios.delete(api('/delete') + id)
       .then(result => {
         location.reload()
       })
